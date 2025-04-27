3d9a39f8adaa89fae2a1f5105209372d7d179b93
vishnukumar1496 <vishece1409@gmail.com>
2025-04-27 11:43:47 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+package week1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age=28;
+		float f=98.3f;
+		System.out.println(f);
+		String name="Vishnu Kumar";
+		System.out.println(name);
+		boolean t = false;
+		System.out.println(t);
+		int regno=2345;
+		System.out.println(age);
+		System.out.println(regno);
+		char value='V';
+		System.out.println(value);
+	}
+
+}
